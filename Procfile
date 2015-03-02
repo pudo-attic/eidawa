4e3903e945cd3c200a0a42bdf4f30848305bd000
Friedrich Lindenberg <friedrich@pudo.org>
2015-03-03 00:08:16 +0100

    begin to get this ready for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn eidawa.wsgi --log-file -
---
+++ eidawa/heroku_settings.py
@@ -0,0 +1,16 @@
+from .settings import * # noqa
+
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+# )
